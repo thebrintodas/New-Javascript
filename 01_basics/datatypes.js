50678c03d921faea888a0569ced366493d6f85e7
thebrintodas <brintodas.cse@gmail.com>
2025-09-25 19:07:12 +0000

    datatypes and var

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; /*treats all js code as newer version*/
+
+// alert(3+3)// we're using node.js, not browser 
+console.log(3+3)
+let name = "brinto"
+let age = 23
+let ismarried = false 
+console.log(typeof "");
+console.log(typeof null); //object
---
+++ 01_basics/var.js
@@ -0,0 +1,9 @@
+const x = 6 /* changing not allowed */
+let accEmail = "brin@gmail.com"
+city = "dhaka"
+city = "rangpur"
+accEmail = "something"
+let accstate
+// x = 7 not allowed as assigned previously
+console.table([x,accEmail,city,accstate])
+

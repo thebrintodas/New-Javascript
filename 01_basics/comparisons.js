04cf11dabfabd3deb2da8c02e6cfaf07ef7245c6
thebrintodas <brintodas.cse@gmail.com>
2025-09-26 05:18:05 +0000

    comparisons

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,19 @@
+console.log(2>1) //True
+console.log("2">1) //true
+
+console.log(null>0) //false
+console.log(null>=0) //true
+console.log(null==0) //false
+
+console.log(undefined>0) //false
+console.log(undefined<0) //false
+console.log(undefined==0) //false
+
+console.log(NaN===NaN) //false
+
+console.log("2"===2) //false
+console.log("2"==2) //true
+
+console.log(false==0) //true
+console.log(false===0) //false
+
